956a55b28c05bfbabbd513e3a2812dcb37d58163
vikas <vv439088@gmail.com>
2023-09-26 17:28:20 +0530

    all sample codes

---
+++ second.py
@@ -0,0 +1,5 @@
+vikas=[12,65,78,10]
+new_vikas=set (vikas)
+new_vikas.remove(max(new_vikas))
+print(new_vikas)
+print(max(new_vikas))
---
+++ swap numbers.py
@@ -0,0 +1,7 @@
+vikas=[12,65,78,10]
+print(vikas)
+position1,position2=0,3
+vikas[position1],vikas[position2]=vikas[position2],vikas[position1]
+print(vikas)
+
+
